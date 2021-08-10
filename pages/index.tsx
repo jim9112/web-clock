033aaa2b3ea7ff7c486ba4771c8e6f81ba50cefd
jim9112 <jim9112@gmail.com>
2021-08-10 15:05:26 -0400

    add assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>Hello</h1>
-      </main>
+      <main>Hi</main>
     </div>
   );
 }
