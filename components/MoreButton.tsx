be29959aa88504054469104175caac46dedbbc27
Jimmy Hannan <jim9112@gmail.com>
2021-08-13 16:58:36 -0400

    basic more button styles

--- components/MoreButton.tsx
+++ components/MoreButton.tsx
@@ -3,11 +3,13 @@ import arrowIcon from '../public/assets/desktop/icon-arrow-up.svg'
 
 const MoreButton = () => {
 return (
-    <button className="relative w-28 grid grid-flow-col items-center bg-white rounded-full" type="button">
+    <button className="relative w-28 p-1 grid grid-flow-col items-center bg-white rounded-full" type="button">
         more
-        <div>
-        <Image className='transform rotate-180 ' src={arrowIcon} alt="arrow icon" />
-        </div>
+
+    <div className="relative grid w-max justify-self-end">
+        <Image className='transform rotate-180 ' objectFit="contain" src={arrowIcon} alt="arrow icon" />
+    </div>
+  
         </button>
 )
 }
